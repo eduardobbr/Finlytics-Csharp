ca85a6de41d27692d59fe1543bd4926c88d3ab94
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2023-09-15 14:30:00 -0300

    fix:debugging initial application

--- Models/Transaction.cs
+++
@@ -1,7 +0,0 @@
-public class Transaction
-{
-    public int Id { get; set; }
-    public string Description { get; set; }
-    public decimal Amount { get; set; }
-    public DateTime Date { get; set; }
-}
